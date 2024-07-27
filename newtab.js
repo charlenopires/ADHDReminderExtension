5ba403d97807f6b69afcad55960e551b24dc4633
Charleno Pires <charleno@gmail.com>
2024-07-26 21:13:19 -0300

    Change password view

--- newtab.js
+++ newtab.js
@@ -10,21 +10,20 @@
 
   document.addEventListener("DOMContentLoaded", function () {
     var reminder = document.getElementById("reminder");
-    reminder.textContent = "*".repeat(10);
+    reminder.textContent = "*".repeat(20);
 
     chrome.storage.local.get(["reminder"], function (result) {
       if (result.reminder) {
         var content = result.reminder;
-        var pass = "*".repeat(content.length);
         var toggler = document.getElementById("toggler");
         toggler.addEventListener("click", function () {
           let reminder = document.querySelector("#reminder");
           if (reminder.getAttribute("data-type") === "password") {
             reminder.setAttribute("data-type", "text");
-            reminder.textContent = pass;
+            reminder.textContent = content;
           } else {
             reminder.setAttribute("data-type", "password");
-            reminder.textContent = content;
+            reminder.textContent = "*".repeat(20);
           }
         });
       }
